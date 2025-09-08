00d15d9b84f587d89890c8122335db54643c08d2
Adebola Rabiu <debolamech@gmail.com>
2025-09-08 11:28:22 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@
 # =========================================
 AFFORDABILITY_PCT = 10           # Fixed cap = 10% of income
 MIN_SCORE = 50                   # Minimum score threshold
-SUPPORTED_LANGS = ["English", "Pidgin"]
+SUPPORTED_LANGS = ["English", "Pidgin", "Yoruba", "Hausa", "Igbo"]
